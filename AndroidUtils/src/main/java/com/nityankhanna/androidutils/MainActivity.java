fb9a41ff83619da30d8acbbba452aba9a6a5daf7
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2013-11-10 21:49:33 -0500

    - fixing merge errors in the MainActivity file

--- AndroidUtils/src/main/java/com/nityankhanna/androidutils/MainActivity.java
+++ AndroidUtils/src/main/java/com/nityankhanna/androidutils/MainActivity.java
@@ -2,16 +2,13 @@ package com.nityankhanna.androidutils;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.nityankhanna.androidutils.security.EncryptionManager;
 
 /**
  * Created by Nityan Khanna on 22/10/13.
  */
-public class MainActivity extends Activity implements View.OnClickListener, OnEncryptionListener {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,32 +26,10 @@ public class MainActivity extends Activity implements View.OnClickListener, OnEn
 		switch (view.getId()) {
 
 			case R.id.button:
-				EncryptionManager encryptionManager = new EncryptionManager(this);
-				encryptionManager.encryptData("Nityan".getBytes());
 				break;
 
 			default:
 				break;
 		}
-	}
-
-	/**
-	 * Called when encrypting data is completed.
-	 *
-	 * @param encryptedData The encrypted data.
-	 */
-	@Override
-	public void onEncryptionCompleted(byte[] encryptedData) {
-		Log.d(Constants.DEBUG, new String(encryptedData));
-	}
-
-	/**
-	 * Called when decrypting data is completed.
-	 *
-	 * @param decryptedData The decrypted data.
-	 */
-	@Override
-	public void onDecryptionCompleted(byte[] decryptedData) {
-		Log.d(Constants.DEBUG, new String(decryptedData));
 	}
 }
