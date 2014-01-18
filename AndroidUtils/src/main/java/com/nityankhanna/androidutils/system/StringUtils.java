e1feb719907ed4cf8e834ae23dc4bfb87fe79507
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2014-01-18 15:40:06 -0500

    - added a StringUtils class containing helper methods for strings

---
+++ AndroidUtils/src/main/java/com/nityankhanna/androidutils/system/StringUtils.java
@@ -0,0 +1,37 @@
+package com.nityankhanna.androidutils.system;
+
+/**
+ * Created by Nityan Khanna on Jan 18 2014.
+ */
+public class StringUtils {
+
+	private StringUtils() {
+	}
+
+	public static boolean isNullOrEmpty(String string) {
+		return string == null || string.trim().isEmpty();
+	}
+
+	public static boolean toBoolean(String string) {
+		return Boolean.valueOf(string);
+	}
+
+	public static double toDouble(String string) {
+		return Double.parseDouble(string);
+	}
+
+	public static float toFloat(String string) {
+		return Float.parseFloat(string);
+	}
+
+	public static int toInt(String string) {
+		return Integer.parseInt(string);
+	}
+
+	public static int wordCount(String string) {
+		String trimmed = string.trim();
+		return trimmed.isEmpty() ? 0 : trimmed.split("\\s+").length;
+	}
+
+
+}
