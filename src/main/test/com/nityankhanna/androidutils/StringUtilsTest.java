e84fc61dbc1c0aa5c336dccc685b1135bed87d28
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2014-08-10 17:32:41 -0400

    - added unit tests for the thread pool class  - added unit tests for the string utils class

--- src/main/test/com/nityankhanna/androidutils/StringUtilsTest.java
+++ src/main/test/com/nityankhanna/androidutils/StringUtilsTest.java
@@ -1,9 +1,68 @@
 package com.nityankhanna.androidutils;
 
-/**
- * Created by Nityan on 2014-08-09.
- */
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.Assert.*;
 
 public class StringUtilsTest
 {
+	@Before
+	public void setUp()
+	{
+
+	}
+
+	@After
+	public void tearDown()
+	{
+
+	}
+
+	@Test
+	public void isNullOrEmpty_ValidData()
+	{
+		Assert.assertTrue(StringUtils.isNullOrEmpty(null));
+		Assert.assertTrue(StringUtils.isNullOrEmpty(""));
+	}
+
+	@Test
+	public void toBoolean_ValidData()
+	{
+		Assert.assertEquals(true, StringUtils.toBoolean("true"));
+	}
+
+	@Test
+	public void toBoolean_InvalidData()
+	{
+		Assert.assertEquals(false, StringUtils.toBoolean("Test"));
+	}
+
+	@Test
+	public void toBoolean_NullData()
+	{
+		Assert.assertEquals(false, StringUtils.toBoolean(null));
+	}
+
+	@Test(expected = UnsupportedEncodingException.class)
+	public void toByteArray_InvalidEncoding() throws UnsupportedEncodingException
+	{
+		StringUtils.toByteArray("Test", "asdfasdfasdf");
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void toByteArray_NullData()
+	{
+		StringUtils.toByteArray(null);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void toByteArray_NullEncoding() throws UnsupportedEncodingException
+	{
+		StringUtils.toByteArray("Test", null);
+	}
 }
--- src/main/test/com/nityankhanna/androidutils/system/async/ThreadPoolTest.java
+++ src/main/test/com/nityankhanna/androidutils/system/async/ThreadPoolTest.java
@@ -1,8 +1,49 @@
 package com.nityankhanna.androidutils.system.async;
 
+import com.nityankhanna.androidutils.StringUtils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.RejectedExecutionException;
+
 /**
  * Created by Nityan on 2014-08-10.
  */
 public class ThreadPoolTest
 {
+	private ThreadPool threadPool;
+
+	@Before
+	public void setUp()
+	{
+		threadPool = ThreadPool.getInstance();
+	}
+
+	@After
+	public void tearDown()
+	{
+		threadPool = null;
+	}
+
+	@Test(expected = RejectedExecutionException.class)
+	public void submit_FullQueue()
+	{
+		for (int i = 0; i < Integer.MAX_VALUE; i++)
+		{
+			threadPool.submit(() -> {
+				for (int j = 0; j < Integer.MAX_VALUE; j++)
+				{
+					StringUtils.toByteArray("Test");
+				}
+			});
+		}
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void submit_NullTask()
+	{
+		threadPool.submit(null);
+	}
 }
